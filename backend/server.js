ec56065d84499848726e9e968dd8c34385c603b2
Ravindra Mohith P <ravindra_mohith@Ravindras-MacBook-Air.local>
2023-12-18 20:54:47 +0530

    (#3) Backend: initialising server and connecting with MongoDB

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then((data) =>
+    console.log(
+      `DB connection successful! Connected with server: ${data.connection.host}`
+    )
+  )
+  .catch((err) => {
+    console.log(`MongoDB connection error: ${err}`);
+  });
+
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
